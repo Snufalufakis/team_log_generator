d90631632cde73410f5fcb45c36701d36f7d575b
Michael Harris <snufalufakis@gmail.com>
2022-07-11 10:08:14 -0700

    just small changes

---
+++ test/Engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require('..lib/engineer');
+
+test("Give Engineer a name to test", () => {
+    const newEmployee = 9000;
+    const e = new Employee("Bodega", newEmployee);
+    expect(e.name).toBe(string);
+  });
+  test("Give the engineer a id to test", () => {
+    const newEmployee = 9000;
+    const e = new Employee("Bodega", newEmployee);
+    expect(e.id).toBe(newEmployee);
+  });
+  test("Give the engineer a email to test", () => {
+    const newEmployee = 9000;
+    const e = new Employee("Bodega", newEmployee);
+    expect(e.email).toBe(newEmployee);
+  });
+  test("Give the engineer a GitHub to test", () => {
+    const newEmployee = 9000;
+    const e = new Employee("Bodega", newEmployee);
+    expect(e.girhub).toBe(newEmployee);
+  });
