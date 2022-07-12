5746222aeeccc57351d2ae1b656fbb4e33222281
Michael Harris <snufalufakis@gmail.com>
2022-07-12 15:08:14 -0700

    fixed spelling with test file

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,20 +1,20 @@
-const Manger = require("../lib/Manger");
+const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 // Testing Office Number
 test("Set the Office Number for arguement sake", () => {
     const newNumber = 1234567;
-    const mangerTest = new Manger("Manny", 30, "MannyBodega@BestBodega.com", newNumber);
-    expect(mangerTest.officeNumber).toBe(newNumber);
+    const managerTest = new Manager("Manny", 30, "MannyBodega@BestBodega.com", newNumber);
+    expect(managerTest.officeNumber).toBe(newNumber);
 })
 //Testing Role
 test("Set the Role for arguement sake", () => {
-    const newRole = "Manger";
-    const mangerTest = new Manger("Manny", 30, "MannyBodega@BestBodega.com", 1234567);
-    expect(mangerTest.getRole()).toBe(newRole);
+    const newRole = "Manager";
+    const managerTest = new Manager("Manny", 30, "MannyBodega@BestBodega.com", 1234567);
+    expect(managerTest.getRole()).toBe(newRole);
 })
 // Verifying Office Number
 test("test the Office Number is right", () => {
     const newNumber = 1234567;
-    const mangerTest = new Manger("Manny", 30, "MannyBodega@BestBodega.com", 1234567);
-    expect(mangerTest.getOfficeNumber()).toBe(newNumber);
+    const managerTest = new Manager("Manny", 30, "MannyBodega@BestBodega.com", 1234567);
+    expect(managerTest.getOfficeNumber()).toBe(newNumber);
 });
